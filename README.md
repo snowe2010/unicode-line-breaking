1c276b276320e15a9c5e929309b9704b04001efd
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-02-13 21:00:39 -0700

    Add build file, readme, license, and gradle 5

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unicode Line Breaking
+
+WIP
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,108 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.3.21"
-}
+val spekVersion = "2.0.0-alpha.2"
 
+plugins {
+    id("com.gradle.build-scan") version "2.1"
+    kotlin("jvm") version "1.3.21"
+    id("nebula.maven-publish") version "9.4.6"
+    id("nebula.publish-verification") version "9.4.6"
+    id("nebula.source-jar") version "9.4.6"
+    id("nebula.release") version "9.2.0"
+    id("nebula.nebula-bintray-publishing") version "5.0.0"
+    jacoco
+}
 group = "com.tylerthrailkill.helpers"
-version = "1.0.0"
 
 repositories {
+    jcenter()
     mavenCentral()
+    maven(url = "https://dl.bintray.com/spekframework/spek-dev/")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+
+    // logging
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.3.0-alpha4")
+    implementation(group = "org.slf4j", name = "slf4j-simple", version = "1.6.1")
+    implementation("io.github.microutils:kotlin-logging:1.6.22")
+
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
+    testImplementation("io.mockk:mockk:1.9.kotlin12")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion") {
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion") {
+        exclude(group = "org.junit.platform")
+        exclude(group = "org.jetbrains.kotlin")
+    }
+    testImplementation("com.beust:klaxon:5.0.1") // used to parse naughty list
+    testImplementation(group = "org.junit.platform", name = "junit-platform-engine", version = "1.3.0-RC1")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<Wrapper> {
+        distributionType = Wrapper.DistributionType.ALL
+    }
+
+    withType<Test> {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
+    }
+    val report = withType<JacocoReport> {
+        reports {
+            xml.apply {
+                isEnabled = true
+                this.destination = file("$buildDir/jacoco/report.xml")
+            }
+            html.apply {
+                isEnabled = true
+                this.destination = file("$buildDir/jacoco/html_report")
+            }
+        }
+        dependsOn("test")
+    }
+    this["check"].dependsOn(report)
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+}
+
+configure<BintrayExtension> {
+    user = findProperty("bintrayUser") as String? ?: System.getenv("BINTRAY_USER")
+    key = findProperty("bintrayKey") as String? ?: System.getenv("BINTRAY_KEY")
+    override = true
+
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        userOrg = "snowe"
+        repo = "maven"
+        name = "unicode-line-breaking"
+        desc = "Unicode line breaking following the Unicode Standard Annex 14 and 29"
+
+        setLicenses("MIT")
+        websiteUrl = "https://github.com/snowe2010/${project.name}"
+        issueTrackerUrl = "https://github.com/snowe2010/${project.name}/issues"
+        vcsUrl = "https://github.com/snowe2010/${project.name}.git"
+        setLabels("axon", "kotlin")
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            gpg(delegateClosureOf<BintrayExtension.GpgConfig> {
+                sign = true
+                passphrase = findProperty("gpgPassphrase") as String? ?: System.getenv("GPG_PASSPHRASE")
+            })
+        })
+    })
+    dryRun = false
+    publish = true
 }
